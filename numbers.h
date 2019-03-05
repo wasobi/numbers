85430609e243069fd5b121d149b2690c6b767534
Sierra Obi <sierraobi@my.uri.edu>
2019-03-05 16:05:41 -0500

    Create numbers.h

---
+++ numbers.h
@@ -0,0 +1,15 @@
+#ifndef __NUMBERS_H__
+#define __NUMBERS_H__
+
+int q1_armstrong(int n);
+int q2_brackets(const char *e);
+int q3_perfect(int x);
+int q4_rotate(int n, int r);
+int q5_str_search(const char *pattern, const char *text);
+int q6_collatz_count(int n);
+double q7_array(int n, const int *A);
+void q8_strings(const char *s1, const char *s2, char *s3);
+void q9_sort(int x, int y, int z, int *list);
+void q10_max_sub_sum(const int *list, int n, int *p, int *q);
+
+#endif
